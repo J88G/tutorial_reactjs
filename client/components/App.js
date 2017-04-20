c907400a49f3298b795f72e3c98a44e9a3bf9d43
jluz <jluz@s21sec.com>
2017-04-20 14:53:05 +0100

    first component

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Greetings from './Greetings';
 
 class App extends React.Component  {
   render (){
     return(
-      <div>
-      <h1>Hello from react hot reloader </h1>
-      </div>
+    <Greetings />
     );
   }
 }
